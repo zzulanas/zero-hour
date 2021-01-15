231edccbdc4cd4cc50db6001ed4afc8dd8c28d23
Zach Zulanas <zachzulanas@Zachs-MacBook-Pro.local>
2021-01-15 13:37:21 -0800

    basic grid layout

---
+++ components/NavBar.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Menu({ fixed }) {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-pink-500">
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+                <a
+                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+                  href="#pablo"
+                >
+                  Zero Hour
+                </a>
+                <button
+                  className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                  type="button"
+                  onClick={() => setMenuOpen(!menuOpen)}
+                >
+                  <i><svg className="h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+</svg></i>
+                </button>
+              </div>
+              <div
+                className={
+                  "lg:flex flex-grow items-center" +
+                  (menuOpen ? " flex" : " hidden")
+                }
+                id="example-navbar-info"
+              >
+                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#pablo"
+                    >
+                      WHO WE ARE
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#pablo"
+                    >
+                      CHAPTERS
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#pablo"
+                    >
+                      OUR PLATFORM
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#pablo"
+                    >
+                      ZERO HOUR 101
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#pablo"
+                    >
+                      DONATE
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="p-0">
       <Head>
-        <title>Create Next App</title>
+        <title>Zero Hour</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <NavBar/>
+      <main className="mx-auto">
+      <div class="grid grid-cols-3 gap-1">
+        <div className="bg-red-700 p-20 col-span-2 row-span-2">1</div>
+        <div className="bg-red-700 p-20">2</div>
+        <div className="bg-red-700 p-20">3</div>
+        <div className="bg-red-700 p-20">4</div>
+        <div className="bg-red-700 p-20">5</div>
+        <div className="bg-red-700 p-20">6</div>
+        <div className="bg-red-700 p-20">7</div>
+        <div className="bg-red-700 p-20">8</div>
+        <div className="bg-red-700 p-20">9</div>
+      </div>
       </main>
 
       <footer className={styles.footer}>
